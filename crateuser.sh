e61ced949be12989371b456bc4225ed34e472692
shadyosama9 <shadyosama554@gmail.com>
2025-05-02 00:56:13 +0300

    increasing user restrictions

--- crateuser.sh
+++ crateuser.sh
@@ -22,7 +22,6 @@ ALLOWED_DIR=/home/"$USERNAME"/allowed_commands
 sudo mkdir -p "$ALLOWED_DIR"
 
 #Create symbolic links to the allowed commands
-
 for cmd in "${COMMANDS[@]}"; do
        if [ -x "/usr/bin/$cmd" ]; then
          sudo ln -s "/usr/bin/$cmd" "/home/$USERNAME/allowed_commands/$cmd"
@@ -33,21 +32,37 @@ for cmd in "${COMMANDS[@]}"; do
 done      
 
 
+# Configure strict environment
 sudo bash <<EOF
-# Set and lock PATH
+# Clean existing configs
+rm -f "/home/$USERNAME"/{.bashrc,.profile}
+
+# Basic restrictions
 echo "export PATH=$ALLOWED_DIR" > "/home/$USERNAME/.profile"
 echo "readonly PATH" >> "/home/$USERNAME/.profile"
 
-# Disable dangerous built-ins
-echo "disable -f set" > "/home/$USERNAME/.bashrc"
-echo "disable -f export" >> "/home/$USERNAME/.bashrc"
-echo "disable -f unset" >> "/home/$USERNAME/.bashrc"
+# Disable all dangerous features
+echo "set -f" > "/home/$USERNAME/.bashrc"                  
+echo "shopt -u extglob" >> "/home/$USERNAME/.bashrc"       
+echo "shopt -u sourcepath" >> "/home/$USERNAME/.bashrc"    
+echo "disable -f command" >> "/home/$USERNAME/.bashrc"     
+echo "disable -f set" >> "/home/$USERNAME/.bashrc"         
+echo "disable -f unset" >> "/home/$USERNAME/.bashrc"       
+echo "alias '\$()=:'" >> "/home/$USERNAME/.bashrc"         
+echo "alias '\\\`=:'" >> "/home/$USERNAME/.bashrc"         
 
-# Set ownership
+# Block shell escapes
+echo "function /bin/bash { return 1; }" >> "/home/$USERNAME/.bashrc"
+echo "function /bin/sh { return 1; }" >> "/home/$USERNAME/.bashrc"
+echo "function /usr/bin/bash { return 1; }" >> "/home/$USERNAME/.bashrc"
+echo "function /usr/bin/sh { return 1; }" >> "/home/$USERNAME/.bashrc"
+
+# Set permissions
 chown -R "$USERNAME:$USERNAME" "/home/$USERNAME"
+chmod 755 "/home/$USERNAME"
 chmod 700 "$ALLOWED_DIR"
+chmod 644 "/home/$USERNAME"/{.bashrc,.profile}
 
-# Make config files immutable
-chattr +a "/home/$USERNAME"/{.bashrc,.profile}
-
+# Final lockdown
+chattr +a "/home/$USERNAME/.bashrc" "/home/$USERNAME/.profile"
 EOF
